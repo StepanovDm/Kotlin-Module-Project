f94874738301a5ee60508bce0ab8d67057678960
дмитрий с <stepanov_dmitriy_ufa@mail.ru>
2024-06-26 19:34:11 +0300

    Ks-1-3

---
+++ src/main/kotlin/Lessson_1/L3.kt
@@ -0,0 +1,13 @@
+package Lessson_1
+
+fun main() {
+    val yearOfFlight = 1961
+    var hour = "09"
+    var minute = "07"
+    println("$yearOfFlight\n$hour\n$minute")
+
+    hour = "10"
+    minute = "55"
+
+    println("$hour:$minute")
+}
