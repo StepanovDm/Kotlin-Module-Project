f8f81ec13725b5d7af5a3b56ad9169ee98b6582b
дмитрий с <stepanov_dmitriy_ufa@mail.ru>
2024-06-27 12:57:11 +0300

    Ks-1-3

---
+++ src/main/kotlin/Lessson_1/L4.kt
@@ -0,0 +1,13 @@
+package Lessson_1
+
+fun main() {
+
+    val lenght: Long = 40_848_600_000
+    val age: Byte = 27
+    val partOfDay: Float = 0.075F
+    val seconds: Int = 6480
+    val partOfYear: Float = 2.0547945205479453E-4F
+    val apogee: Int = 327000
+
+    println("$lenght\n$age\n$partOfDay\n$seconds\n$partOfYear\n$apogee")
+}
