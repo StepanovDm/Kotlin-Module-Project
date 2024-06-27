9877dc6b9be984201cdf652b3b08c0666a844261
дмитрий с <stepanov_dmitriy_ufa@mail.ru>
2024-06-27 22:07:16 +0300

    Ks-2-1

---
+++ src/main/kotlin/Lesson_2/L1.kt
@@ -0,0 +1,14 @@
+package Lesson_2
+
+fun main() {
+
+    val ball_1 = 3.0
+    val ball_2 = 4.0
+    val ball_3 = 3.0
+    val ball_4 = 5.0
+
+    val totalBall = (ball_1 + ball_2 + ball_3 + ball_4)/4.0
+
+    println(totalBall)
+
+}
