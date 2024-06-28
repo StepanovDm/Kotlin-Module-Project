e631e4f38b24a33c3fb35086a931ddff1ce538fb
дмитрий с <stepanov_dmitriy_ufa@mail.ru>
2024-06-28 13:12:57 +0300

    l_1_2

---
+++ src/main/kotlin/L_1_2.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val ordersOnWeb = 75
+    val textAfterPurchase =  "Благодарим за заказ, ждем Вас вновь!"
+    println(ordersOnWeb)
+    println(textAfterPurchase)
+
+
+    var workers = 2000
+    //println(workers)
+
+    workers = 1999
+    println(workers)
+}
